b41d98b8050f0d802a9ccb86ea517ef037569440
Peter Hollows <peter.hollows@gmail.com>
2015-05-08 11:52:50 +1000

    rails new ey-c3-email-signatures --skip-bundle -O -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EyC3EmailSignatures::Application.config.secret_token = 'bf0e54e47be1831321e7fc530a8dd60dfd54e9756f9ab3497fd910c97fcedcd2cadae96fddb3d66c3bbc1243ed56d3f15885b65329c77030cd7b868cc0284a55'

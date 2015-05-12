b4b1a6d0332226eb35cecb427e359d22393b87c0
Peter Hollows <peter.hollows@gmail.com>
2015-05-12 12:21:31 +1000

    Add feedback link for creating tickets with embedded params

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def feedback_url
+    desc = "Please add your feedback here.\n\n--- debugging info ---\n#{Base64.encode64 params.to_json}"
+    query = { 'ticket[subject]' => 'Signature Feedback', 'ticket[description]' => desc }
+    uri = URI::HTTPS.build(host: 'c3products.zendesk.com', path: '/anonymous_requests/new', query: query.to_query)
+
+    uri.to_s
+  end
+
 end
